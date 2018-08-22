cf9bee0835b4dc017631eb41f7da6f1a17af7462
Unknown <regine.f.marte@student.uts.edu.au>
2018-08-22 21:16:15 +1000

    Registration activity

--- app/src/main/java/com/example/regineyo/healthconnect/MainActivity.java
+++ app/src/main/java/com/example/regineyo/healthconnect/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.regineyo.healthconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameET;
+    private EditText passwordET;
+    private Button forgotPasswordBtn;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
+        usernameET = findViewById(R.id.usernameET);
+        passwordET = findViewById(R.id.passwordET);
+
+
     }
+
 }
