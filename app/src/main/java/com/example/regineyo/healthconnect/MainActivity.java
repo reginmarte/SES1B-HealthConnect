7cffb98b141df86a85296596b8aaf1e0dee8caaf
Daniel <danieljacob9123@gmail.com>
2018-08-28 00:47:48 +1000

    Draft GUI

--- app/src/main/java/com/example/regineyo/healthconnect/MainActivity.java
+++ app/src/main/java/com/example/regineyo/healthconnect/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.regineyo.healthconnect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
