34164c439c89f5baebe0a3e3e8ddfad5e2292c07
Unknown <regine.f.marte@student.uts.edu.au>
2018-09-04 16:59:28 +1000

    users successfully registered to firebase

--- app/src/main/java/com/example/regineyo/healthconnect/MainActivity.java
+++ app/src/main/java/com/example/regineyo/healthconnect/MainActivity.java
@@ -1,48 +1,43 @@
 package com.example.regineyo.healthconnect;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextInputEditText emailET;
-    private TextInputEditText passwordET;
-    private Button forgotPasswordBtn;
-    private Button loginBtn;
+    private static final String TAG = "MainActivity" ;
+    private TextInputEditText emailET, passwordET;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
         emailET = findViewById(R.id.emailET);
         passwordET = findViewById(R.id.passwordET);
 
+        //user selects login
         findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextInputLayout til = findViewById(R.id.TIL_email);
-                TextInputLayout til2 = findViewById(R.id.TIL_password);
-                til.setError(null);
-                til2.setError(null);
-
-                // THIS CHECKS FOR VALID INPUT
-                if (emailET.getText().toString().trim().isEmpty()
-                        || !Patterns.EMAIL_ADDRESS.matcher(emailET.getText().toString().trim()).matches()) {
-                    til.setError("Please enter a valid email");
-                    return;
-                }
-                if (passwordET.getText().toString().trim().isEmpty()) {
-                    til2.setError("Please enter password");
-                    return;
-                }
-                goToMain();
+                checkInputs();
             }
         });
 
@@ -59,18 +54,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.forgotPasswordBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
             }
         });
     }
 
-    //user goes to homepage
-    public void goToMain() {
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
-    }
 
     @Override
     public void onClick(View v) {
-
     }
+
+    public void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Login failed",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Intent intent = new Intent(this, HomePage.class);
+            startActivity(intent);
+        }
+    }
+
+    public void checkInputs() {
+        TextInputLayout til = findViewById(R.id.TIL_email);
+        TextInputLayout til2 = findViewById(R.id.TIL_password);
+        til.setError(null);
+        til2.setError(null);
+
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        // checks for valid input
+        if (email.isEmpty()
+                || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            til.setError("Please enter a valid email");
+            emailET.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            til2.setError("Please enter password");
+            passwordET.requestFocus();
+            return;
+        }
+
+        login(email, password);
+    }
+
 }
--- app/src/main/java/com/example/regineyo/healthconnect/RegistrationActivity.java
+++ app/src/main/java/com/example/regineyo/healthconnect/RegistrationActivity.java
@@ -32,21 +32,18 @@ import java.util.GregorianCalendar;
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private static final String TAG = "RegistrationActivity";
-    private TextInputEditText nameET;
-    private TextInputEditText emailET;
-    private TextInputEditText numberET;
-    private TextInputEditText passwordET;
-    private TextInputEditText confirmPasswordET;
+    private TextInputEditText nameET, emailET, numberET, passwordET, heightET, weightET;
     private TextView dateOfBirthTV;
-    private TextInputEditText heightET;
-    private TextInputEditText weightET;
-    private DatePicker date;
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
 
         nameET = findViewById(R.id.nameET);
         emailET = findViewById(R.id.emailET);
@@ -59,89 +56,118 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         RadioButton maleBtn = findViewById(R.id.maleRB);
         maleBtn.setChecked(true);
 
-        findViewById(R.id.submitBtn).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.confirmBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth = FirebaseAuth.getInstance();
-
-                nameET.setError(null);
-                emailET.setError(null);
-                numberET.setError(null);
-                passwordET.setError(null);
-                confirmPasswordET.setError(null);
-                dateOfBirthTV.setError(null);
-                heightET.setError(null);
-                weightET.setError(null);
-
-                String email = emailET.getText().toString().trim();
-                String password = passwordET.getText().toString().trim();
-
-                if(nameET.getText().toString().trim().isEmpty()){
-                    nameET.setError("Please enter name");
-                    nameET.requestFocus();
-                    return;
-                }
-
-                if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){   // THIS METHOD CHECKS IF ITS A REAL EMAIL
-                    emailET.setError("Invalid email");
-                    emailET.requestFocus();
-                    return;
-                }
-
-                if(numberET.getText().toString().trim().isEmpty()
-                        || numberET.getText().length() < 8){
-                    numberET.setError("Invalid number");
-                    numberET.requestFocus();
-                    return;
-                }
-                if(password.isEmpty()){
-                    passwordET.setError("Please enter password");
-                    passwordET.requestFocus();
-                    return;
-                }
-
-                if(dateOfBirthTV.getText().toString().equalsIgnoreCase("day/month/year")) {
-                    dateOfBirthTV.setError("Please select date of birth");
-                    return;
-                }
-
-                if(heightET.getText().toString().trim().isEmpty()
-                        || Integer.parseInt(heightET.getText().toString().trim()) <= 0){
-                    heightET.setError("Please enter height");
-                    heightET.requestFocus();
-                    return;
-                }
-                if(weightET.getText().toString().trim().isEmpty()
-                        || Integer.parseInt(weightET.getText().toString().trim()) <= 0){
-                    weightET.setError("Please enter weight");
-                    weightET.requestFocus();
-                    return;
-                }
-
-//                mAuth.createUserWithEmailAndPassword(email, password)
-//                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<AuthResult> task) {
-//                                if (task.isSuccessful()) {
-//                                    Toast.makeText(RegistrationActivity.this, "User Registered",
-//                                            Toast.LENGTH_SHORT).show();
-//                                    goToMain();
-//                                    // Sign in success, update UI with the signed-in user's information
-//                                } else if(task.getException() instanceof FirebaseAuthUserCollisionException){
-//                                    // If sign in fails, display a message to the user.
-//                                    Toast.makeText(RegistrationActivity.this, "User Exists",
-//                                            Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        }
-
-                goToMain();
+                checkInputs();
             }
         });
+
     }
 
     @Override
     public void onClick(View v) {
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void checkInputs() {
+        TextInputLayout nameTIL = findViewById(R.id.TIL_name);
+        TextInputLayout emailTIL = findViewById(R.id.TIL_email);
+        TextInputLayout numberTIL = findViewById(R.id.TIL_number);
+        TextInputLayout passwordTIL = findViewById(R.id.TIL_password);
+        TextInputLayout heightTIL = findViewById(R.id.TIL_height);
+        TextInputLayout weightTIL = findViewById(R.id.TIL_weight);
+
+        nameTIL.setError(null);
+        emailTIL.setError(null);
+        numberTIL.setError(null);
+        passwordTIL.setError(null);
+        dateOfBirthTV.setError(null);
+        heightTIL.setError(null);
+        weightTIL.setError(null);
+
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        if(nameET.getText().toString().trim().isEmpty()){
+            nameTIL.setError("Please enter your name");
+            nameET.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){   // THIS METHOD CHECKS IF ITS A REAL EMAIL
+            emailTIL.setError("Please enter a valid email");
+            emailET.requestFocus();
+            return;
+        }
+
+        if(numberET.getText().toString().trim().isEmpty()
+                || numberET.getText().length() < 8){
+            numberTIL.setError("Please enter a valid contact number");
+            numberET.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            passwordTIL.setError("Please enter a password");
+            passwordET.requestFocus();
+            return;
+        }
+
+        if(dateOfBirthTV.getText().toString().equalsIgnoreCase("day/month/year")) {
+            dateOfBirthTV.setError("Please select date of birth");
+            return;
+        }
+
+        if(heightET.getText().toString().trim().isEmpty()
+                || Integer.parseInt(heightET.getText().toString().trim()) <= 0){
+            heightTIL.setError("Please enter your height");
+            heightET.requestFocus();
+            return;
+        }
+
+        if(weightET.getText().toString().trim().isEmpty()
+                || Integer.parseInt(weightET.getText().toString().trim()) <= 0){
+            weightTIL.setError("Please enter your weight");
+            weightET.requestFocus();
+            return;
+        }
+
+        createAccount(email, password);
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegistrationActivity.this, "Registration failed",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Intent intent = new Intent(this, HomePage.class);
+            startActivity(intent);
+        }
     }
 
     public void showDatePickerDialog(View v) {
@@ -161,21 +187,13 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     }
 
     public static class DatePickerFragment extends DialogFragment {
-
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             final Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
-
             return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year,month, day);
         }
     }
-
-    public void goToMain() {
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
-    }
-
 }
